83af4d6732c87c02b3b9d76ef8ed575783aab4c3
chimo599 <mehrichaima@gmail.com>
2022-04-24 23:49:08 +0100

    last commit

---
+++ .history/src/components/BabySitter/archive_20220424171723.vue
@@ -0,0 +1,226 @@
+<template>
+  <div>
+    <Header />
+    <div class="container">
+      <div class="main-body">
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          crossorigin="anonymous"
+        />
+
+        <div class="row gutters-sm">
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <div class="card-body">
+                <div class="d-flex flex-column align-items-center text-center">
+                  <img
+                    src="https://bootdey.com/img/Content/avatar/avatar8.png"
+                    alt="Admin"
+                    class="rounded-circle"
+                    width="150"
+                    border-radius="50%"
+                  />
+
+                  <div class="mt-3">
+                    <h4>John Doe</h4>
+                    <p class="text-secondary mb-1">student</p>
+
+                    <button class="btn btn-outline-primary">Message</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-8">
+            <div class="card mb-3">
+              <div class="card-body">
+                <h5>list of demands</h5>
+                <div class="row" v-for="offer in offer" :key="offer.id">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Name</h6>
+                  </div>
+                  <div class="col-sm-9 text-secondary">
+                    {{ offer.username }}
+                  </div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Duration</h6>
+                  </div>
+                  <div class="col-sm-9 text-secondary">{{ offer.duration }}</div>
+                </div>
+
+                <hr />
+                <div class="row">
+                  <div class="col-sm-3">
+                    <h6 class="mb-0">Note</h6>
+                  </div>
+                  <div class="col-sm-9 text-secondary">
+                    {{ offer.Note }}
+                  </div>
+                </div>
+                <hr />
+                <div class="row">
+                  <div class="col-sm-12">
+                  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Header from "@/components/header/header.vue";
+import axios from "axios";
+export default {
+  name: "profile",
+  components: { Header },
+  data: () => {
+    return {
+      user: [],
+      offer: [
+        {
+         
+        },
+      ],
+      offerForm: {},
+    };
+  },
+  mounted(){
+axios.get("http://localhost:5000/api/babysitter/offers/"+localStorage.getItem('id'))
+.then((response) => {
+    this.offer=response.data;
+       
+        })
+        .catch((reason) => {
+          console.log("the error is ", reason);
+        });
+  },
+  methods: {
+    Edit(id) {
+      console.log("edit" + id);
+    },
+  },
+};
+</script>
+<style scoped>
+body {
+  margin-top: 20px;
+
+  text-align: left;
+}
+.container {
+  position: relative;
+  top: 70px;
+}
+.main-body {
+  padding: 15px;
+}
+.card {
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+}
+
+.card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 0 solid rgba(0, 0, 0, 0.125);
+  border-radius: 0.25rem;
+}
+
+.card-body {
+  flex: 1 1 auto;
+  min-height: 1px;
+  padding: 1rem;
+}
+
+.gutters-sm {
+  margin-right: -8px;
+  margin-left: -8px;
+}
+
+.gutters-sm > .col,
+.gutters-sm > [class*="col-"] {
+  padding-right: 8px;
+  padding-left: 8px;
+}
+.mb-3,
+.my-3 {
+  margin-bottom: 1rem !important;
+}
+
+.bg-gray-300 {
+  background-color: #e2e8f0;
+}
+.h-100 {
+  height: 100% !important;
+}
+.shadow-none {
+  box-shadow: none !important;
+}
+body {
+  margin-top: 20px;
+  background: #eee;
+}
+@media (min-width: 0) {
+  .g-mr-15 {
+    margin-right: 1.07143rem !important;
+  }
+}
+@media (min-width: 0) {
+  .g-mt-3 {
+    margin-top: 0.21429rem !important;
+  }
+}
+
+.g-height-50 {
+  height: 50px;
+}
+
+.g-width-50 {
+  width: 50px !important;
+}
+
+@media (min-width: 0) {
+  .g-pa-30 {
+    padding: 2.14286rem !important;
+  }
+}
+
+.g-bg-secondary {
+  background-color: #fafafa !important;
+}
+
+.u-shadow-v18 {
+  box-shadow: 0 5px 10px -6px rgba(0, 0, 0, 0.15);
+}
+
+.g-color-gray-dark-v4 {
+  color: #777 !important;
+}
+
+.g-font-size-12 {
+  font-size: 0.85714rem !important;
+}
+
+.media-comment {
+  margin-top: 20px;
+}
+</style>
